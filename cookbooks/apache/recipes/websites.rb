7a7282a4013174ac93693185ed40a073583495b5
jordi <luk206@gmail.com>
2018-05-21 10:24:31 +0000

    add some updates

--- cookbooks/apache/recipes/websites.rb
+++ cookbooks/apache/recipes/websites.rb
@@ -1,4 +1,10 @@
 file 'default www' do
   path '/var/www/html/index.html'
-  content 'Hello world!'
+  content 'Hello world! version 2.0.0'
+end
+
+webnodes = search('node', 'role:web')
+
+webnodes.each do |node|
+  puts node
 end
